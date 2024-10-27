464659d9bad033117257c41b2449f12240ad5ce5
Rodagency10 <rodriguepuh@gmail.com>
2024-10-27 19:17:11 +0000

    correction in tests repo

--- __tests__/MovieItem.test.tsx
+++ __tests__/MovieItem.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import MovieItem from '../src/components/MovieItem';
 import '@testing-library/jest-dom';
